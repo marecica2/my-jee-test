bf65e2045451e7fcdcc981d5685b75dfcede051c
Marek <marecica2@gmail.com>
2015-10-09 18:13:52 +0200

    transaction manager working

--- src/main/java/org/bmsource/dao/GenericDao.java
+++ src/main/java/org/bmsource/dao/GenericDao.java
@@ -5,15 +5,24 @@ import java.lang.reflect.ParameterizedType;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Transactional
 public abstract class GenericDao<T, ID extends Serializable> {
 
 	private Class<T> entityClass;
+
+	@Inject
+	private PlatformTransactionManager transactionManager;
 
 	protected abstract EntityManager getEntityManager();
 
@@ -37,9 +46,16 @@ public abstract class GenericDao<T, ID extends Serializable> {
 		return getEntityManager().find(entityClass, id);
 	}
 
-	public T update(T t) {
-		t = getEntityManager().merge(t);
-		return t;
+	@Transactional(propagation = Propagation.NEVER)
+	public T update(final T t) {
+		final TransactionTemplate transactionTemplate = new TransactionTemplate(transactionManager);
+		return transactionTemplate.execute(new TransactionCallback<T>() {
+			@Override
+			public T doInTransaction(TransactionStatus paramTransactionStatus) {
+				T result = getEntityManager().merge(t);
+				return result;
+			}
+		});
 	}
 
 	public void delete(T t) {
