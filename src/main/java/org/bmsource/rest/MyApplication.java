c455003152bb93b34412f5828ebf43e089de2745
Marek <marecica2@gmail.com>
2015-10-11 22:01:12 +0200

    jersey

--- src/main/java/org/bmsource/rest/MyApplication.java
+++ src/main/java/org/bmsource/rest/MyApplication.java
@@ -1,16 +1,15 @@
-// package org.bmsource.rest;
-//
-// import org.bmsource.dao.BookDao;
-// import org.glassfish.jersey.server.ResourceConfig;
-//
-// import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-//
-// public class MyApplication extends ResourceConfig {
-//
-// public MyApplication() {
-// packages("org.bmsource.rest");
-// register(BookDao.class);
-// register(MyResource.class);
-// register(JacksonJsonProvider.class);
-// }
-// }
+package org.bmsource.rest;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
+public class MyApplication extends ResourceConfig {
+
+	public MyApplication() {
+		packages("org.bmsource.rest");
+		register(MyResource.class);
+		register(JacksonJsonProvider.class);
+
+	}
+}
